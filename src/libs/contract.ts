57b4b1ede1a4e3e19afbed7a2cbb53524743474f
Junho Yeo <i@junho.io>
2023-11-22 19:18:21 +0900

    fix: use `ContractRunner` over `ISigner` in `Contract`

--- src/libs/contract.ts
+++ src/libs/contract.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import { ContractRunner, ethers } from 'ethers';
 import { ISigner } from '../types';
 
 /**
@@ -8,6 +8,6 @@ import { ISigner } from '../types';
  * @param {array} abi - The abi of the contract.
  */
 
-export const Contract = (contractAddress: string, abi: Array<any>, signer: ISigner) => {
-  return new ethers.Contract(contractAddress, abi, signer);
+export const Contract = (contractAddress: string, abi: Array<any>, runner: ContractRunner) => {
+  return new ethers.Contract(contractAddress, abi, runner);
 };
